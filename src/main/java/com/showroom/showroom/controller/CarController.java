9f8607024f655316b1727be447745f9bde5c855c
Ali Al-Abbas <alisadco99@gmail.com>
2023-07-28 23:33:02 -0400

    Added code to API

--- src/main/java/com/showroom/showroom/controller/CarController.java
+++ src/main/java/com/showroom/showroom/controller/CarController.java
@@ -1,4 +1,39 @@
 package com.showroom.showroom.controller;
 
+@RestController
+@RequestMapping("/cars")
 public class CarController {
+    // Implement CRUD operations for Car entity
+}
+
+@RestController
+@RequestMapping("/dealerships")
+public class DealershipController {
+    // Implement CRUD operations for Dealership entity
+
+    @GetMapping("/by-zipcode")
+    public List<Dealership> getDealershipsByZipCode(@RequestParam String zipCode) {
+        // Implement logic to get dealerships within a certain zip code and radius
+    }
+
+    @GetMapping("/by-cars")
+    public List<Dealership> getDealershipsByCarMake(@RequestParam String carMake) {
+        // Implement logic to get dealerships by the kinds of cars they sell
+    }
+
+    @GetMapping("/{dealershipId}/reviews")
+    public List<Review> getDealershipReviews(@PathVariable Long dealershipId) {
+        // Implement logic to get consumer ratings and reviews of a dealership by its ID
+    }
+}
+
+@RestController
+@RequestMapping("/reviews")
+public class ReviewController {
+    // Implement CRUD operations for Review entity
+
+    @GetMapping("/by-cars-and-zipcode")
+    public List<Review> getReviewsByCarMakeAndZipCode(@RequestParam String carMake, @RequestParam String zipCode) {
+        // Implement logic to get consumer ratings and reviews of dealerships by their car makes and the zip code they’re in
+    }
 }
--- src/main/java/com/showroom/showroom/repository/CarRepository.java
+++ src/main/java/com/showroom/showroom/repository/CarRepository.java
@@ -1,5 +1,6 @@
 package com.showroom.showroom.repository;
 
-public interface CarRepository extends JpaRepository<Car, Integer> {
-    Optional<Car> findByBrand(String brand);
+public interface CarRepository extends JpaRepository<Car, Long> {
+    List<Car> findByYearAndMileageAndFeatureAndOptions(int year, int mileage, String feature, String options);
+    // Other custom queries
 }
