1b5fae714b3edbe35f48af8e04056b58960c0532
Ali Al-Abbas <alisadco99@gmail.com>
2023-07-31 23:00:38 -0400

    Updated App.js

--- showroom-frontend/src/App.js
+++ showroom-frontend/src/App.js
@@ -4,13 +4,13 @@ import CarList from './components/CarList';
 import Footer from './components/Footer';
 
 function App() {
-  return (
-      <div className="app">
-        <Header />
-        <CarList />
-        <Footer />
-      </div>
-  );
+    return (
+        <div className="app">
+            <Header />
+            <CarList />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
