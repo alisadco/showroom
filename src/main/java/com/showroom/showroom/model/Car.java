2265cf2b30d1ad3e08c690e05b6835470c7bf0b3
Ali Al-Abbas <alisadco99@gmail.com>
2023-08-07 22:29:02 -0400

    Finished car and car search objects

--- src/main/java/com/showroom/showroom/model/Car.java
+++ src/main/java/com/showroom/showroom/model/Car.java
@@ -2,11 +2,93 @@ package com.showroom.showroom.model;
 
 @Entity
 public class Car {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
+    private String make;
+    private String model;
     private int year;
+    private String color;
     private int mileage;
-    // Other car attributes, getters, and setters
+    private String vin;
+    private List<String> packages; // List of optional packages
+    private List<String> options;  // List of additional options
+
+    // Constructors
+    public Car() {
+    }
+
+    public Car(String make, String model, int year, String color, int mileage, String vin,
+               List<String> packages, List<String> options) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+        this.mileage = mileage;
+        this.vin = vin;
+        this.packages = packages;
+        this.options = options;
+    }
+
+    // Getters and Setters
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(int mileage) {
+        this.mileage = mileage;
+    }
+
+    public String getVin() {
+        return vin;
+    }
+
+    public void setVin(String vin) {
+        this.vin = vin;
+    }
+
+    public List<String> getPackages() {
+        return packages;
+    }
+
+    public void setPackages(List<String> packages) {
+        this.packages = packages;
+    }
+
+    public List<String> getOptions() {
+        return options;
+    }
+
+    public void setOptions(List<String> options) {
+        this.options = options;
+    }
 }
--- src/main/java/com/showroom/showroom/model/Filter.java
+++
@@ -1,7 +0,0 @@
-package com.showroom.showroom.model;
-
-public class Filter {
-    private String field;
-    private String value;
-    // Getters and setters
-}
